08b36a316c48193456d14e516d4d0a08e260ce44
beichen515 <maoss993@newschool.edu>
2023-02-15 21:40:06 -0500

    add-chapter2&chenge-chapter1

---
+++ chapter1.js
@@ -0,0 +1,12 @@
+$("#w1").click(function(){
+    $(".p3").slideToggle();
+  });
+  $("#w2").click(function(){
+    $(".p2").slideToggle();
+  });
+  $("#w3").click(function(){
+    $(".p4").slideToggle();
+  });
+  $("#w4").click(function(){
+    $(".p5").slideToggle();
+  });
